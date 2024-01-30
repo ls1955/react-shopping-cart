923b4b7e850fbf475641a9d1e20804c35719f077
ls1955 <xxlsxx1955@gmail.com>
2024-01-30 20:46:53 +0800

    Implement CardItem functionality
    Style wise, for now the CardItem should have everything we need...
    Next is creating... an App?

--- src/item-card.jsx
+++ src/item-card.jsx
@@ -1,7 +1,35 @@
-export default function ItemCard() {
+import PropTypes from "prop-types";
+
+export default function ItemCard({
+  title,
+  quantity,
+  isInCart = false,
+  onAdd,
+  onRemove,
+}) {
   return (
     <div>
-      <input type="number" name="quantity" id="quantity" defaultValue="1" />
+      <p>{title}</p>
+      <input
+        type="number"
+        name="quantity"
+        id="quantity"
+        defaultValue="1"
+        value={quantity}
+      />
+      {isInCart ? (
+        <button onClick={onRemove}>Remove</button>
+      ) : (
+        <button onClick={onAdd}>Add</button>
+      )}
     </div>
   );
 }
+
+ItemCard.propTypes = {
+  title: PropTypes.string,
+  quantity: PropTypes.number,
+  isInCart: PropTypes.bool,
+  onAdd: PropTypes.func,
+  onRemove: PropTypes.func,
+};
--- tests/item-card.test.jsx
+++ tests/item-card.test.jsx
@@ -1,12 +1,76 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import ItemCard from "../src/item-card";
 
+// setup props
+const title = "tuna";
+const onAdd = vi.fn();
+const onRemove = vi.fn();
+const defaultProps = { title, onAdd, onRemove, isInCart: false };
+
 describe("item card", () => {
-  it("by default have a quantity of 1", () => {
-    render(<ItemCard />);
+  it("displays the given title", () => {
+    render(<ItemCard {...defaultProps} />);
+
+    expect(screen.queryByText(title)).toBeInTheDocument();
+  });
+
+  it("has a quantity of 1 by default", () => {
+    render(<ItemCard {...defaultProps} />);
 
     expect(screen.getByRole("spinbutton").value).toEqual("1");
+  });
+
+  it("quantity can be set via prop", () => {
+    render(<ItemCard {...defaultProps} quantity={100}/>)
+
+    expect(screen.getByRole("spinbutton").value).toEqual("100")
+  })
+
+  it("changes quantity via user input", async () => {
+    const user = userEvent.setup();
+
+    render(<ItemCard {...defaultProps} />);
+
+    await user.clear(screen.getByRole("spinbutton"));
+    await user.type(screen.getByRole("spinbutton"), "999");
+
+    expect(screen.getByRole("spinbutton").value).toEqual("999");
+  });
+
+  it("displays an add button when not in cart", () => {
+    render(<ItemCard {...defaultProps} />);
+
+    expect(screen.queryByRole("button", { name: /add/i })).toBeInTheDocument();
+  });
+
+  it("calls onAdd handler when user clicked add button", async () => {
+    const user = userEvent.setup();
+
+    render(<ItemCard {...defaultProps} />);
+
+    await user.click(screen.getByRole("button", { name: /add/i }));
+
+    expect(onAdd).toHaveBeenCalledOnce();
+  });
+
+  it("displays a remove button when in cart", () => {
+    render(<ItemCard {...defaultProps} isInCart />);
+
+    expect(
+      screen.queryByRole("button", { name: /remove/i })
+    ).toBeInTheDocument();
+  });
+
+  it("calls onRemove handler when user clicked remove button", async () => {
+    const user = userEvent.setup();
+
+    render(<ItemCard {...defaultProps} isInCart />);
+
+    await user.click(screen.getByRole("button", { name: /remove/i }));
+
+    expect(onRemove).to.toHaveBeenCalledOnce();
   });
 });
