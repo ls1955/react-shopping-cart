691b6a7c3c04a3f7c51fbf801cb09e79252b4f5e
ls1955 <xxlsxx1955@gmail.com>
2024-02-05 10:57:23 +0800

    Wrap up NavigationBar
    Style wise, functionalities.... hopefully are sufficient.
    Guess its finally time to test the page, then finally opening up the
    browser.

--- src/navigation-bar.jsx
+++ src/navigation-bar.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { CartContext } from "./contexts/cart-context";
+
 export default function NavigationBar() {
+  const { cart } = useContext(CartContext);
+  const addedItemCount = Object.values(cart).filter(({ isInCart }) => isInCart).length
+
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/items">Items</Link>
-      <Link to="cart">Cart</Link>
+      <Link to="cart">Cart ({addedItemCount})</Link>
     </nav>
   );
 }
--- tests/navigation-bar.test.jsx
+++ tests/navigation-bar.test.jsx
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
 import NavigationBar from "../src/navigation-bar";
+import renderWithCartProvider from "./helpers/render-with-cart-provider";
 
 describe("navigation bar", () => {
   it("has a link for homepage", () => {
@@ -30,5 +31,29 @@ describe("navigation bar", () => {
 
     expect(cartPageLink).toBeInTheDocument();
     expect(cartPageLink).toHaveAttribute("href", "/cart");
+  });
+
+  it("has a cartPageLink that show 0 item if no item is in cart", () => {
+    render(<NavigationBar />, { wrapper: BrowserRouter });
+
+    expect(screen.queryByRole("link", { name: /0/ })).toBeInTheDocument();
+  });
+
+  it("has a cartPageLink that show added item amount in cart", () => {
+    const value = {
+      cart: {
+        tuna: { isInCart: true },
+        ham: { isInCart: true },
+        pentarou: { isInCart: true },
+        yam: { isInCart: false },
+      },
+    };
+
+    renderWithCartProvider(<NavigationBar />, {
+      providerProps: { value },
+      wrapper: BrowserRouter,
+    });
+
+    expect(screen.queryByRole("link", { name: /3/ })).toBeInTheDocument();
   });
 });
