b6eb22c7dd9b4cf1cc8174f18adfa38ae8f014c6
Cheong <xxlsxx1955@gmail.com>
2024-02-06 22:04:56 +0800

    Initial implementation
    Well, it is indeed an experience. Doing TDD when at the highway...
    Anyhow, the functionality should be more or less complete.  Probably.

--- src/components/cart-item.jsx
+++ src/components/cart-item.jsx
@@ -1,1 +1,29 @@
-export default function CartItem() {}
+import { useContext } from "react";
+import PropTypes from "prop-types";
+
+import { CartContext } from "../contexts/cart-context";
+
+export default function CartItem({ title }) {
+  const { cart, setCart } = useContext(CartContext);
+  const { quantity = 1 } = cart?.[title] ?? {};
+
+  const handleUpdate = ({ target: { value } }) => {
+    // NOTE: What if user manually enter a value <= 0?
+    setCart({ ...cart, [title]: { quantity: value, isInCart: true } });
+  };
+  const handleRemove = () => {
+    setCart({ ...cart, [title]: { quantity, isInCart: false } });
+  };
+
+  return (
+    <div>
+      <p>{title}</p>
+      <input type="number" value={quantity} min={1} onChange={handleUpdate} />
+      <button onClick={handleRemove}>Remove</button>
+    </div>
+  );
+}
+
+CartItem.propTypes = {
+  title: PropTypes.string.isRequired,
+};
--- tests/components/cart-item.test.jsx
+++ tests/components/cart-item.test.jsx
@@ -12,45 +12,46 @@ describe("cart item", () => {
     expect(screen.queryByText("tuna")).toBeInTheDocument();
   });
 
-  it.skip("shows quantity from CartProvider", () => {
+  it("shows quantity from CartProvider", () => {
     const value = { cart: { tuna: { quantity: 999 } } };
 
     renderWithCartProvider(<CartItem title="tuna" />, {
       providerProps: { value },
     });
 
-    expect(screen.queryByText("999")).toBeInTheDocument();
+    expect(
+      screen.queryByRole("spinbutton", { value: 999 })
+    ).toBeInTheDocument();
   });
 
-  it.skip("calls setCart from CartProvider after update quantity field", async () => {
+  it("calls setCart from CartProvider after update quantity field", async () => {
     const user = userEvent.setup();
     const value = { cart: {}, setCart: vi.fn() };
 
     renderWithCartProvider(<CartItem title="tuna" />, {
       providerProps: { value },
     });
-
     await user.type(screen.getByRole("spinbutton"), "999");
 
     expect(value.setCart).toBeCalled();
   });
 
-  it.skip("shows a remove button", () => {
+  it("shows a remove button", () => {
     render(<CartItem title="tuna" />);
 
-    expect(screen.queryByText(/x/i)).toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: /remove/i })
+    ).toBeInTheDocument();
   });
 
-  it.skip("calls setCart from CartProvider after clicked remove button", async () => {
+  it("calls setCart from CartProvider after clicked remove button", async () => {
     const user = userEvent.setup();
     const value = { cart: {}, setCart: vi.fn() };
 
     renderWithCartProvider(<CartItem title="tuna" />, {
       providerProps: { value },
     });
-
-    // NOTE: ensure we are selecting the correct button, since there are multiple button.
-    await user.click(screen.getByRole("spinbutton"));
+    await user.click(screen.getByRole("button", { name: /remove/i }));
 
     expect(value.setCart).toBeCalled();
   });
