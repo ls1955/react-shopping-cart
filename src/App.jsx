8154a04bfe5c500010437ba4fc07dbc827685323
Cheong <xxlsxx1955@gmail.com>
2024-02-10 20:38:20 +0800

    Move router from main.jsx to App
    As App will provide context to subsequent pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/home";
+
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
 export default function App() {
-  // TODO: 
+  return <RouterProvider router={router} />;
+
+  return;
+  // TODO:
   // * Load cart's data via useEffect
-  // * Pass CartContext here 
+  // * Pass CartContext here
   // * Create Router
 }
