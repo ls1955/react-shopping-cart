13acbf54514c36ce2ba91f24f39fe59789aeeae2
Cheong <xxlsxx1955@gmail.com>
2024-02-10 20:56:33 +0800

    Insert rest of routes

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./routes/home";
+import ItemsPage from "./routes/items-page";
+import CartPage from "./routes/cart-page";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/items", element: <ItemsPage /> },
+  { path: "/cart", element: <CartPage /> },
+]);
 
 export default function Router() {
   return <RouterProvider router={router} />;
