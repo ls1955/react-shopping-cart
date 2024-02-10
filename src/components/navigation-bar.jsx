7a1f79333adff93413120af346d46f71d3ab3dfe
Cheong <xxlsxx1955@gmail.com>
2024-02-10 21:29:28 +0800

    Include styles

--- src/components/navigation-bar.jsx
+++ src/components/navigation-bar.jsx
@@ -2,16 +2,17 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { CartContext } from "../contexts/cart-context";
+import styles from "../styles.module.css"
 
 export default function NavigationBar() {
   const { cart } = useContext(CartContext);
   const addedItemCount = Object.values(cart).filter(({ isInCart }) => isInCart).length
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/items">Items</Link>
-      <Link to="/cart">Cart ({addedItemCount})</Link>
+    <nav className={styles.navbar}>
+      <Link to="/" className={styles.navLink}>Home</Link>
+      <Link to="/items" className={styles.navLink}>Items</Link>
+      <Link to="/cart" className={styles.navLink}>Cart ({addedItemCount})</Link>
     </nav>
   );
 }
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,12 @@
-import NavigationBar from "../components/navigation-bar"
+import NavigationBar from "../components/navigation-bar";
+
+import styles from "../styles.module.css";
 
 export default function Home() {
   return (
     <>
       <NavigationBar />
-      <h1>Welcome to the store</h1>
+      <h1 className={styles.heading}>Welcome to the store</h1>
     </>
   );
 }
